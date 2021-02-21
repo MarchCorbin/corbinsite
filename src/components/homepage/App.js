378d8bc32aff288ffb8d39060d381e264c37c708
Corbin March <marchcorbin@gmail.com>
2021-02-21 15:56:55 -0700

    Added hash functionality to handle refresh error

--- src/components/homepage/App.js
+++ src/components/homepage/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, HashRouter} from 'react-router-dom'
 import HomePage from './HomePage'
 import Contact from '../contact/Contact'
 import Experience from '../experience/Experience'
@@ -7,12 +7,12 @@ import Projects from '../projects/Projects'
 
 const App = () => {
   return (
-  <BrowserRouter>
-  <Route path='/projects' component={Projects} />
-  <Route exact path='/contact' component={Contact} />
-  <Route path='/experience' component={Experience} />
-  <Route exact path='/' component={HomePage} />
-  </BrowserRouter>
+  <HashRouter>
+    <Route path='/projects' component={Projects} />
+    <Route exact path='/contact' component={Contact} />
+    <Route path='/experience' component={Experience} />
+    <Route exact path='/' component={HomePage} />
+  </HashRouter>
   )
 }
 export default App
